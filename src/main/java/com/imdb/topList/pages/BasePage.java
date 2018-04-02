30572cd7b685e218cf2f20d369e0f0a6381488d5
LENOVO\USER <rohit639u@gmail.com>
2018-04-03 05:14:11 +0530

    added log

--- src/main/java/com/imdb/topList/pages/BasePage.java
+++ src/main/java/com/imdb/topList/pages/BasePage.java
@@ -1,5 +1,6 @@
 package com.imdb.topList.pages;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -8,12 +9,15 @@ import org.testng.Assert;
 import com.imdb.topList.utils.BaseWebdriver;
 import com.imdb.topList.utils.FunctionLib;
 import com.imdb.topList.utils.Locators;
+import com.imdb.topList.utils.LoggerClass;
 
 public class BasePage {
 
 	@FindBy(id = Locators.BasePage.homeLoGoLocator)
 	WebElement homeLogo;
-
+	
+	protected static final Logger logger =LoggerClass.createLogger();
+	
 	public BasePage() {
 		PageFactory.initElements(BaseWebdriver.getDriver(), this);
 		Assert.assertTrue(FunctionLib.isElemntVisble(homeLogo), "Failed as home logo is not visiable on the screen");
