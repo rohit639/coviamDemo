0ff62512160a4a928ef234995674a85fa6228713
rohit kumar <rkumar22@apple.com>
2018-04-11 15:22:36 +0530

    Changes made for testng.xml

--- src/test/java/com/imdb/topList/RedPandaDemoTest.java
+++ src/test/java/com/imdb/topList/RedPandaDemoTest.java
@@ -8,13 +8,13 @@ import com.imdb.topList.utils.SqLiteDB;
 
 public class RedPandaDemoTest extends BaseWebdriver {
 
-	@Test(enabled = true,alwaysRun=true,priority=1)
+	@Test(enabled = true,alwaysRun=true,priority=1,groups= {"smokeTest","regression"})
 	public void gettingAllRatitngs() {
 		LandingPage homePage = new LandingPage();
 		homePage.navigateToTopList().getAllList();
 	}
 
-	@Test(enabled = true, alwaysRun=true,priority=2)
+	@Test(enabled = true, alwaysRun=true,priority=2,groups= {"smokeTest","regression"})
 	public void printingAllResults() {
 		SqLiteDB.printResult();
 	}
